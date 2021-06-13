ee164107fcfdacde3f05a440fb206a20d825e551
Ganesh <ganeshgavhad9@gmail.com>
2021-06-13 17:43:40 +0530

    Added : Function of Last Node Deletion

--- com/bridgelabz/linkedlist/Runner.java
+++ com/bridgelabz/linkedlist/Runner.java
@@ -5,16 +5,18 @@ public class Runner<T> {
 	public static void main(String[] args) {
 		
 		LinkedList <Integer> List = new LinkedList<Integer>();
-		
+		System.out.println("Before deletion First node");
 		List.add(30);
 		List.add(70);
 		List.add(30);
 		List.AtStart(25);
 		List.AddAtIndex(2,32);
 		List.show();
-		System.out.println("Before deletion First node");
 		List.deleteFirst();
-		System.out.println("Before deletion First node");
+		System.out.println("After deletion First node");
+		List.show();
+		List.deleteLast();
+		System.out.println("Last Node Deleted");
 		List.show();
 
 	}
